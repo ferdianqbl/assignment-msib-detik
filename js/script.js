b81b308d4acef9ea393974011e7d46966653ebaa
ferdianqbl <muhammadferdianiqbal@gmail.com>
2023-07-22 13:21:09 +0700

    create navbar

--- js/script.js
+++ js/script.js
@@ -1,30 +1,37 @@
 $(document).ready(function () {
-    $(".owl-carousel").owlCarousel({
+  // Closes responsive menu when a scroll trigger link is clicked
+  $(".navbar-toggler").click(function () {
+    // $(".navbar-collapse").collapse("hide");
+    $(".navbar-toggler--nav").toggleClass("toggler-active");
+  });
+
+  // CAROUSEL
+  $(".owl-carousel").owlCarousel({
+    loop: true,
+    margin: 10,
+    responsiveClass: true,
+    dots: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    autoplayHoverPause: true,
+    nav: true,
+    navText: [
+      "<i class='bi bi-chevron-left'></i>",
+      "<i class='bi bi-chevron-right'></i>",
+    ],
+    responsive: {
+      0: {
+        items: 1,
+        nav: false,
+      },
+      768: {
+        items: 2,
+        nav: false,
+      },
+      992: {
+        items: 3,
         loop: true,
-        margin: 10,
-        responsiveClass: true,
-        dots: true,
-        autoplay: true,
-        autoplayTimeout: 3000,
-        autoplayHoverPause: true,
-        nav: true,
-        navText: [
-            "<i class='bi bi-chevron-left'></i>",
-            "<i class='bi bi-chevron-right'></i>",
-        ],
-        responsive: {
-            0: {
-                items: 1,
-                nav: false,
-            },
-            768: {
-                items: 2,
-                nav: false,
-            },
-            992: {
-                items: 3,
-                loop: true,
-            },
-        },
-    });
+      },
+    },
+  });
 });
